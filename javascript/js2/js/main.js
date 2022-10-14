27dd1bbee2899e5deb0b9491622afba4ace18077
strawberryfru1t <12010430@sfcollege.edu>
2022-10-14 00:06:06 -0400

    lesson 3

---
+++ javascript/js2/js/main.js
@@ -0,0 +1,15 @@
+// Strings
+const myVariable = "Mathematics";
+
+// length property
+console.log("every good boy does fine".length);
+
+// string methods
+console.log(myVariable.charAt(6));
+console.log(myVariable.indexOf("at"));
+console.log(myVariable.lastIndexOf("ath"));
+console.log(myVariable.slice(5, 8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("mat"));
+console.log(myVariable.split(""));
