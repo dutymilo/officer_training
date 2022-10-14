3fd07c115e86599359dc42eba25f6ea09ab0d78f
strawberryfru1t <12010430@sfcollege.edu>
2022-10-14 00:42:39 -0400

    lesson 5

---
+++ javascript/js5/js/main.js
@@ -0,0 +1,13 @@
+// Math Methods and Properties
+
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(3.5));
+console.log(Math.ceil(3.2));
+console.log(Math.floor(3.5));
+console.log(Math.pow(2, 4));
+console.log(Math.min(2, 4, 75));
+console.log(Math.max(2, 4, 75));
+
+// From 1 - 10
+console.log(Math.floor(Math.random() * 10) + 1);
