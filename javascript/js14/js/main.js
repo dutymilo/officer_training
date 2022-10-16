0454aced4c60c5689e12b711ccef7de70d2c01eb
strawberryfru1t <12010430@sfcollege.edu>
2022-10-15 22:22:46 -0400

    lesson 13

---
+++ javascript/js14/js/main.js
@@ -0,0 +1,27 @@
+// var, let, const
+
+// global scope
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+function myFunc() {
+  var x = 10;
+  const z = 5;
+  {
+    var x = 11; // function scoped
+    const z = 6; // block scoped
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
+  }
+  console.log(`funciton: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
+}
+
+myFunc();
