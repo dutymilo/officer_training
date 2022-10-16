2ec957137b24d1ff066e60302dc3cdccb7587e5a
strawberryfru1t <12010430@sfcollege.edu>
2022-10-15 20:24:16 -0400

    lesson 11

---
+++ javascript/js11/js/main.js
@@ -0,0 +1,42 @@
+// interactive game
+
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame) {
+  // play game
+  let playerChoice = prompt("Please enter rock, paper, or scissors");
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (
+      playerOne === "rock" ||
+      playerOne === "paper" ||
+      playerOne === "scissors"
+    ) {
+      let computerChoice = Math.floor(Math.random() * 3 + 1);
+      let computer =
+        computerChoice === 1
+          ? "rock"
+          : computerChoice === 2
+          ? "paper"
+          : "scissors";
+      let result =
+        playerOne === computer
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nTie game!`
+          : playerOne === "rock" && computer === "paper"
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          : playerOne === "paper" && computer === "sicssors"
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          : playerOne === "scissors" && computer === "rock"
+          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+      alert(result);
+      let playAgain = confirm("Play Again?");
+      playAgain ? location.reload() : alert("Ok, thanks for playing.");
+    } else {
+      alert("You didn't enter rock, paper, or scissors.");
+    }
+  } else {
+    alert("I guess you changed your mind. Maybe next time.");
+  }
+} else {
+  alert("Ok, maybe next time.");
+}
